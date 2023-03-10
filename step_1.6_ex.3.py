6d8e795aaa0c837cf6ed5703886b546e4d2b9388
iljah <gusarev353@gmail.com>
2023-03-10 18:38:00 +0300

    Add new step without driver in project local folder.

---
+++ step_1.6_ex.3.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+link = 'http://suninjuly.github.io/huge_form.html'
+
+try:
+    chromeOptions = webdriver.ChromeOptions()
+    chromeOptions.add_argument("--no-sandbox")
+    browser = webdriver.Chrome(chrome_options=chromeOptions)
+
+    browser.get(link)
+
+    elements = browser.find_elements(By.TAG_NAME, "input")
+    for element in elements:
+        element.send_keys("Мой ответ")
+
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button.click()
+
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(30)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+# не забываем оставить пустую строку в конце файла
