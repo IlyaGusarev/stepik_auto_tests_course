5bd38b287d6bcb0d9981396dd9b1e585d6d3287f
IlyaGusarev <gusarev353@gmail.com>
2023-05-16 10:33:48 +0300

    Add new step.

---
+++ section_3/test_lesson_5_step_3_not_or_and_markers.py
@@ -0,0 +1,32 @@
+# pytest -s -v -m "not smoke" test_lesson_5_step_3_not_or_and_markers.py
+# pytest -s -v -m "smoke or regression" test_lesson_5_step_3_not_or_and_markers.py
+# pytest -s -v -m "smoke and win10" test_lesson_5_step_3_not_or_and_markers.py
+
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+
+@pytest.fixture(scope="function")
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
+
+class TestMainPage1:
+
+    @pytest.mark.smoke
+    def test_guest_should_see_login_link(self, browser):
+        browser.get(link)
+        browser.find_element(By.CSS_SELECTOR, "#login_link")
+
+    @pytest.mark.smoke
+    @pytest.mark.win10
+    def test_guest_should_see_basket_link_on_the_main_page(self, browser):
+        browser.get(link)
+        browser.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")
