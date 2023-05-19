21ff89efc500ef73317d8768364e0127e2f01371
IlyaGusarev <gusarev353@gmail.com>
2023-05-19 10:30:28 +0300

    Add class TestLogin with parametrize decorator.

--- section_3/test_lesson_6_step_3_parametrize.py
+++ section_3/test_lesson_6_step_3_parametrize.py
@@ -6,3 +6,14 @@ def test_guest_should_see_login_link(browser, language):
     link = f"http://selenium1py.pythonanywhere.com/{language}/"
     browser.get(link)
     browser.find_element(By.CSS_SELECTOR, "#login_link")
+
+
+# Можно задавать параметризацию также для всего тестового класса, чтобы все тесты в классе запустились с
+# заданными параметрами. В таком случае отметка о параметризации должна быть перед объявлением класса:
+@pytest.mark.parametrize('language', ["ru", "en-gb"])
+class TestLogin:
+    def test_guest_should_see_login_link(self, browser, language):
+        link = f"http://selenium1py.pythonanywhere.com/{language}/"
+        browser.get(link)
+        browser.find_element(By.CSS_SELECTOR, "#login_link")
+        # этот тест запустится 2 раза
