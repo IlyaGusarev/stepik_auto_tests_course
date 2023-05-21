cdfa1d9b752990a2cb3061688e8a412c79adaeaf
IlyaGusarev <gusarev353@gmail.com>
2023-05-21 12:45:15 +0300

    Modified conftest.py for choose webdriver.

--- conftest.py
+++ conftest.py
@@ -1,10 +1,36 @@
+import os
 import pytest
 from selenium import webdriver
+from selenium.webdriver.firefox.service import Service as FirefoxService
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        '--browser_name',
+        action = 'store',
+        default = 'chrome',
+        help = 'Choose browser'
+    )
+
 
 @pytest.fixture(scope="function")
-def browser():
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome()
+def browser(request):
+    browser_name = request.config.getoption('browser_name')
+    browser = None
+    if browser_name == 'chrome':
+        browser = webdriver.Chrome()
+    elif browser_name == 'firefox':
+        install_dir = "/snap/firefox/current/usr/lib/firefox"
+        driver_loc = os.path.join(install_dir, "geckodriver")
+        binary_loc = os.path.join(install_dir, "firefox")
+        service = FirefoxService(driver_loc)
+        opts = webdriver.FirefoxOptions()
+        opts.binary_location = binary_loc
+        opts.set_preference("dom.webdriver.enabled", False)
+        browser = webdriver.Firefox(service=service, options=opts)
+    else:
+        raise pytest.UsageError(
+            '--browser_name should be chrome or firefox'
+        )
     yield browser
-    print("\nquit browser..")
     browser.quit()
